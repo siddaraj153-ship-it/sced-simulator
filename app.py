99ff9a37209fa57b5ac505d8c18f045d75f0ab0b
siddaraj153-ship-it <siddaraj153@gmail.com>
2026-06-25 17:29:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
-import streamlit as st
-import pandas as pd
-from solver import solve_ed
 from pyomo.environ import *
 from pyomo.contrib.appsi.solvers import Highs
 
 
 def solve_ed(load, generators):
-    """
-    generators = [
-        {"pmin": 50, "pmax": 300, "cost": 100},
-        {"pmin": 50, "pmax": 400, "cost": 150}
-    ]
-    """
-
     n = len(generators)
 
     model = ConcreteModel()
@@ -20,18 +10,15 @@ def solve_ed(load, generators):
     model.G = RangeSet(0, n - 1)
     model.P = Var(model.G, within=NonNegativeReals)
 
-    # Objective
     model.obj = Objective(
         expr=sum(generators[g]["cost"] * model.P[g] for g in model.G),
         sense=minimize
     )
 
-    # Balance constraint
     model.balance = Constraint(
         expr=sum(model.P[g] for g in model.G) == load
     )
 
-    # Generator limits
     def gen_limits_rule(model, g):
         return (generators[g]["pmin"], model.P[g], generators[g]["pmax"])
 
@@ -40,10 +27,7 @@ def solve_ed(load, generators):
     solver = Highs()
     solver.solve(model)
 
-    dispatch = []
+    dispatch = [value(model.P[g]) for g in model.G]
     total_cost = value(model.obj)
-
-    for g in model.G:
-        dispatch.append(value(model.P[g]))
 
     return dispatch, total_cost
